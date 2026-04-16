3153a21032835802f0afb2fb7eebb7c9903f1ba5
daniellacosse <3759828+daniellacosse@users.noreply.github.com>
2026-04-16 19:16:14 -0400

    run deno fmt

--- format/bench.tsx
+++ format/bench.tsx
@@ -11,21 +11,25 @@ Deno.bench(
   },
 );
 
-Deno.bench(`react-dom/server - 10000 rows`, { group: "10000 rows" }, (bench) => {
-  const rows = Array.from({ length: 10000 }, (_, i) => ({
-    id: `row-${i}`,
-    class: i % 2 === 0 ? "even" : "odd",
-    content: `Row #${i}`,
-  }));
-  bench.start();
-  renderToString(
-    <div>
-      {rows.map((row) => (
-        <div key={row.id} id={row.id} className={row.class}>
-          {row.content}
-        </div>
-      ))}
-    </div>,
-  );
-  bench.end();
-});
+Deno.bench(
+  `react-dom/server - 10000 rows`,
+  { group: "10000 rows" },
+  (bench) => {
+    const rows = Array.from({ length: 10000 }, (_, i) => ({
+      id: `row-${i}`,
+      class: i % 2 === 0 ? "even" : "odd",
+      content: `Row #${i}`,
+    }));
+    bench.start();
+    renderToString(
+      <div>
+        {rows.map((row) => (
+          <div key={row.id} id={row.id} className={row.class}>
+            {row.content}
+          </div>
+        ))}
+      </div>,
+    );
+    bench.end();
+  },
+);
